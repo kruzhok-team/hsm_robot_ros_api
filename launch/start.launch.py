4ed7001764c57fa3806c5d8ca07c0b15366ab42c
Alexey Fedoseev <aleksey@fedoseev.net>
2026-08-01 13:25:15 +0300

    add package launcher

---
+++ launch/start.launch.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    debug_node = Node(
+        package='hsm_robot',
+        executable='debug_node',
+        name='debug_node'
+    )
+    timer_node = Node(
+        package='hsm_robot',
+        executable='timer_node',
+        name='timer_node'
+    )
+    wheels_node = Node(
+        package='hsm_robot',
+        executable='wheels_node',
+        name='wheels_node'
+    )
+    navigation_node = Node(
+        package='hsm_robot',
+        executable='navigation_node',
+        name='navigation_node'
+    )
+
+    return LaunchDescription([
+        debug_node, timer_node, wheels_node, navigation_node
+    ])
